5674e9d1e04975f36e4562715fbafc08f15a1854
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2016-07-10 20:48:39 +0200

    Disallow dynamic attributes in SWIG wrappers

--- production/examples/swig_demo/sample.i
+++ production/examples/swig_demo/sample.i
@@ -1,5 +1,9 @@
 %module sample
 
+%feature("python:nondynamic", "1");
+// Otherwise you can add arbitrary new attributes to swigged classes' __dict__,
+// and even assign to read-only properties.
+
 %include "typemaps.i"
 %include "std_vector.i"
 %include "std_string.i"
--- production/examples/swig_demo/swig_tests.py
+++ production/examples/swig_demo/swig_tests.py
@@ -1,3 +1,4 @@
+import pytest
 from production.examples.swig_demo import sample
 
 
@@ -5,3 +6,7 @@ def test_stuff():
     assert sample.N == 42
     assert sample.square_float(2) == 4
     assert sample.reverse([1, 2, 3]) == (3, 2, 1)
+
+    hz = sample.Hz()
+    with pytest.raises(AttributeError):
+        hz.zzz = 0
