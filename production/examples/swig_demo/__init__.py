790c82df9e19a728034c3ca827d3cf70eec4621e
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2016-07-15 22:20:50 +0200

    Automatically rebuild extensions on configuration changes

--- production/examples/swig_demo/__init__.py
+++ production/examples/swig_demo/__init__.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import json
 import distutils.core
 import distutils.util
 
@@ -19,19 +20,26 @@ def build_extension():
             '-O0', '-D_GLIBCXX_DEBUG', '-D_GLIBCXX_DEBUG_PEDANTIC']
         undef_macros = ['NDEBUG']  # want assertions
 
-    # TODO: force distutils to rebuild the extension when the environment
-    # variable changes, even if the source was not touched.
-
     cur_dir = os.getcwd()
     try:
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+        # Distutils looks at the timestamps to determine whether the extension
+        # should be rebuilt. We want it rebuilt when compiler options change,
+        # even if no source file is touched.
+        config = '// generated\n' + json.dumps(dict(
+            CC=os.getenv('CC'),
+            extra_compile_args=extra_compile_args,
+            undef_macros=undef_macros), sort_keys=True, indent='  ')
+        update_file_content_if_changed('_config.json', config)
+
         distutils.core.setup(
             name='sample',
             py_modules=['sample'],
             ext_modules=[
                 distutils.core.Extension('_sample',
                     ['sample.i', 'sample.cpp'],
-                    depends=['sample.h', '__init__.py'],
+                    depends=['sample.h', '__init__.py', '_config.json'],
                     swig_opts=['-c++'],
                     extra_compile_args=extra_compile_args,
                     undef_macros=undef_macros,
@@ -41,6 +49,17 @@ def build_extension():
         )
     finally:
         os.chdir(cur_dir)
+
+
+def update_file_content_if_changed(filename, content):
+    if os.path.exists(filename):
+        with open(filename) as f:
+            old_content = f.read()
+    else:
+        old_content = None
+    if content != old_content:
+        with open(filename, 'w') as f:
+            f.write(content)
 
 
 # contextlib.redirect_stdout is not enough because spawned processes
