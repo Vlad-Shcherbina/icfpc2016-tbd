8b9b48f060cc999d361f2cc93d47a2753bc04e58
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2016-08-05 07:04:15 +0200

    Point on edge test

--- production/cg.py
+++ production/cg.py
@@ -3,7 +3,7 @@ Basic computational geometry stuff.
 """
 
 from fractions import Fraction
-from typing import NamedTuple, List
+from typing import NamedTuple, List, Tuple
 
 
 Point = NamedTuple('Point', [('x', Fraction), ('y', Fraction)])
@@ -17,3 +17,25 @@ def polygon_area(vertices: List[Point]) -> Fraction:
         s += (pt1.x - pt2.x) * (pt1.y + pt2.y)
 
     return s / Fraction(2)
+
+
+def is_point_on_edge(pt: Point, edge: Tuple[Point, Point]) -> bool:
+    pt1, pt2 = edge
+    assert pt1 != pt2
+
+    if pt.x < pt1.x and pt.x < pt2.x:
+        return False
+    if pt.y < pt1.y and pt.y < pt2.y:
+        return False
+    if pt.x > pt1.x and pt.x > pt2.x:
+        return False
+    if pt.y > pt1.y and pt.y > pt2.y:
+        return False
+
+    dx = pt1.x - pt2.x
+    dy = pt1.y - pt2.y
+
+    dxp = pt.x - pt1.x
+    dyp = pt.y - pt1.y
+
+    return dx * dyp == dy * dxp
--- production/cg_tests.py
+++ production/cg_tests.py
@@ -1,7 +1,10 @@
 #!/bin/env python3
 
 from fractions import Fraction
-from production.cg import Point, polygon_area
+
+import pytest
+
+from production.cg import Point, polygon_area, is_point_on_edge
 
 
 def test_area():
@@ -19,6 +22,23 @@ def test_area():
         Point(1, Fraction(1, 3)),
     ]
     assert polygon_area(tr) == -Fraction(1, 3)
+
+
+@pytest.mark.parametrize("edge", [
+    (Point(0, 0), Point(2, 1)),
+    (Point(2, 1), Point(0, 0)),
+])
+def test_point_on_edge(edge):
+    assert is_point_on_edge(Point(0, 0), edge)
+    assert is_point_on_edge(Point(2, 1), edge)
+    assert is_point_on_edge(Point(Fraction(2, 3), Fraction(1, 3)), edge)
+
+    assert not is_point_on_edge(Point(1, 2), edge)
+    assert not is_point_on_edge(
+        Point(Fraction(31, 30), Fraction(19, 30)), edge)
+    assert not is_point_on_edge(Point(4, 2), edge)
+    assert not is_point_on_edge(Point(-2, -1), edge)
+
 
 if __name__ == '__main__':
     import sys, logging, pytest
