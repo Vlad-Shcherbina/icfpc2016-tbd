7b2fae316577e98c6fc5bc174867ec6d84f28c03
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2016-08-05 07:41:16 +0200

    Point-in-poly test

--- production/cg.py
+++ production/cg.py
@@ -39,3 +39,31 @@ def is_point_on_edge(pt: Point, edge: Tuple[Point, Point]) -> bool:
     dyp = pt.y - pt1.y
 
     return dx * dyp == dy * dxp
+
+
+def count_revolutions(pt: Point, poly: List[Point]) -> int:
+    """The number of revolutions the polygon "makes" around the point.
+
+    If the point is outside of the poly, 0.
+    If the point is inside CCW poly, +1.
+    If the point is inside CW poly, -1.
+    """
+    edges = list(zip(poly, poly[1:] + poly[:1]))
+    assert all(not is_point_on_edge(pt, edge) for edge in edges), (
+        ('undefined because the point is on the border', pt, poly))
+
+    cnt = 0
+    for pt1, pt2 in edges:
+        if min(pt1.y, pt2.y) <= pt.y < max(pt1.y, pt2.y):
+            t = (pt.y - pt1.y) / Fraction(pt2.y - pt1.y)
+            print(pt, pt1, pt2, t)
+            assert 0 <= t <= 1
+            x = pt1.x + t * (pt2.x - pt1.x)
+            assert x != pt.x
+            if x > pt.x:
+                if pt1.y < pt2.y:
+                    cnt += 1
+                else:
+                    cnt -= 1
+
+    return cnt
--- production/cg_tests.py
+++ production/cg_tests.py
@@ -4,17 +4,20 @@ from fractions import Fraction
 
 import pytest
 
-from production.cg import Point, polygon_area, is_point_on_edge
+from production.cg import (
+    Point, polygon_area, is_point_on_edge, count_revolutions)
+
+
+unit_square = [
+    Point(0, 0),
+    Point(1, 0),
+    Point(1, 1),
+    Point(0, 1),
+]
 
 
 def test_area():
-    sq = [
-        Point(0, 0),
-        Point(1, 0),
-        Point(1, 1),
-        Point(0, 1),
-    ]
-    assert polygon_area(sq) == 1
+    assert polygon_area(unit_square) == 1
 
     tr = [
         Point(Fraction(1, 2), 0),
@@ -38,6 +41,27 @@ def test_point_on_edge(edge):
         Point(Fraction(31, 30), Fraction(19, 30)), edge)
     assert not is_point_on_edge(Point(4, 2), edge)
     assert not is_point_on_edge(Point(-2, -1), edge)
+
+
+def test_count_revolutions():
+    with pytest.raises(AssertionError) as exc_info:
+        count_revolutions(Point(1, Fraction(1, 3)), unit_square)
+    assert 'on the border' in str(exc_info.value)
+
+    assert count_revolutions(
+        Point(Fraction(1, 2), Fraction(1, 2)), unit_square) == 1
+    assert count_revolutions(
+        Point(Fraction(1, 2), Fraction(1, 2)), unit_square[::-1]) == -1
+
+    assert count_revolutions(
+        Point(Fraction(3, 2), Fraction(1, 2)), unit_square) == 0
+    assert count_revolutions(
+        Point(Fraction(1, 2), Fraction(3, 2)), unit_square) == 0
+
+    assert count_revolutions(Point(2, 0), unit_square) == 0
+    assert count_revolutions(Point(-2, 0), unit_square) == 0
+    assert count_revolutions(Point(2, 1), unit_square) == 0
+    assert count_revolutions(Point(-2, 1), unit_square) == 0
 
 
 if __name__ == '__main__':
