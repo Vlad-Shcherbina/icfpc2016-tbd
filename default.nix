9f9edad25316cda560b68144858771130bef1b2f
Jonn Mostovoy <amarr.industrial@gmail.com>
2016-08-05 15:30:38 +0300

    Nixify project
    Please, ping @manpages when you add something to requirements.txt
    from now on, he'll reflect those dependencies in the default.nix
    and make sure that tests pass on Nix.

---
+++ default.nix
@@ -0,0 +1,32 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  py = pkgs.python35Packages;
+
+  f = { mkDerivation    ? pkgs.stdenv.mkDerivation,
+        python          ? pkgs.python3,
+        pillow          ? py.pillow,
+        pytest          ? py.pytest,
+        pytestcov       ? py.pytestcov,
+        swig3           ? pkgs.swig3,
+        twisted         ? py.twisted }:
+
+    mkDerivation {
+      name = "tbd-tournament-runner";
+      version = "0.1.0.0";
+      src = ./.;
+      buildInputs = [ python
+                      pillow
+                      pytest
+                      pytestcov
+                      swig3
+                      twisted ]; };
+
+  drv = pkgs.callPackage f {};
+
+in
+
+  drv
