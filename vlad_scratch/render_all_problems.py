16ec641432bc893bdc38b27a4176feb368c817b4
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2016-08-05 18:19:40 +0200

    Script to render 'all' problems

---
+++ vlad_scratch/render_all_problems.py
@@ -0,0 +1,25 @@
+import glob
+import os
+import re
+
+from production import ioformats
+from production import render
+
+
+def main():
+    path = os.path.join(__file__, '..', '..', 'manpages_scratch', '*.problem')
+    path = os.path.normpath(path)
+    print(path)
+
+    for filename in glob.glob(path):
+        m = re.search(r'-(\d+).problem', os.path.basename(filename))
+        print(filename)
+        with open(filename) as fin:
+            problem = ioformats.parse_problem(fin.read())
+            im = render.render_polys_and_edges(
+                problem.silhouette, problem.skeleton)
+            im.save('{}.png'.format(m.group(1)))
+
+
+if __name__ == '__main__':
+    main()
