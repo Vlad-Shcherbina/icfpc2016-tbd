dc243cfee97c6c49e5879ae394c59ac99bd77a27
peluche <me@peluche.im>
2016-08-07 00:22:52 +0200

    Improve quadratic solution generator

--- production/quadratic_skeleton.py
+++ production/quadratic_skeleton.py
@@ -9,35 +9,55 @@ import itertools
 def get_points(poly):
     return list(itertools.chain.from_iterable(poly.edges))
 
-def make_flap(poly, i, flap_size):
+def make_flap(poly, i, flap_size, delta_fold_width):
     points = get_points(poly)
-    delta = i * flap_size / 10
-    
     botl = min(points, key=lambda p: (p.y, p.x))
-    return Edge(Point(botl.x + i * flap_size + 2 * delta, botl.y),
-                Point(botl.x + i * flap_size + delta, botl.y + 1))
+    delta = 0 if i else flap_size
+    delta += delta_fold_width * i
+    return Edge(Point(botl.x + delta + flap_size, botl.y),
+                Point(botl.x + delta, botl.y + 1))
 
-def fold_at_45(polys):
+def fold_at_45(polys, eps=Fraction(-1, 2)):
     points = get_points(polys[-1])
-    botl = min(points, key=lambda p: (p.y, p.x))
-    rtop = max(points, key=lambda p: (p.x, p.y))
-    return fold(polys, Edge(Point(botl.x, (botl.y + rtop.y) / 2), rtop))
+    botr = min(points, key=lambda p: (p.y, -p.x))
+    return fold(polys, Edge(Point(botr.x - 1, botr.y + eps),
+                            Point(botr.x, botr.y + 1 + eps)))
 
-def quadratic_skeleton(n=10, flap_size=Fraction(1, 100)):
+def fold_at_45_2(polys):
+    eps = Fraction(1, 50)
+    return fold(polys, Edge(Point(Fraction(1), Fraction(1, 2) + eps),
+                            Point(Fraction(3, 2), Fraction(0) + eps)))
+
+def quadratic_skeleton(n):
+    flap_size = Fraction(1, n + 4)
+    delta_fold_width = flap_size / (2 * n ** 2)
     polys = [unitsq]
     print(polys[-1])
     for i in range(n):
-        e = make_flap(polys[-1], i, flap_size)
+        e = make_flap(polys[-1], i, flap_size, delta_fold_width)
         polys = fold(polys, e)
-    # 45 degre fold
     polys = fold_at_45(polys)
+    polys = fold_at_45_2(polys)
     return polys
 
 if __name__ == '__main__':
     from production.render import render_polys_and_edges
-    fold = quadratic_skeleton()
-    res = write_fold(fold)
-    # x = ioformats.solution_to_str(res)
+
+    sizes = []
+    for i in range(1, 15):
+    # for i in [11]:
+        polys = quadratic_skeleton(n=i)
+        print('*' * 50)
+        res = write_fold(polys)
+        print('*' * 50)
+        # x = ioformats.solution_to_str(res)
+        size = ioformats.solution_size(ioformats.solution_to_str(res))
+        sizes.append((i, size))
+        print('size {}'.format(size))
+        
+        im = render.render_solution(res, size=2000)
+        im.save('img/x{:>02}.png'.format(i))
+
+    for i, size in sizes:
+        print('{:>02}: {:>5}'.format(i, size))
     
-    im = render.render_solution(res, size=2000)
-    im.save('img/x.png')
