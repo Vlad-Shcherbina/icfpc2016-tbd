8005ab2520481b3b1d48ae9c411312be5ad1d9de
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2016-08-06 16:43:01 +0200

    Switch to new and shiny (on the outside) Renderer

--- production/render.py
+++ production/render.py
@@ -4,68 +4,132 @@ import random
 from PIL import Image, ImageDraw
 
 from production.cg import Point, polygon_area
-from production.ioformats import load_problem
+from production import ioformats
+
+
+class Renderer:
+    """Responsible for drawing one single square image with polygons.
+
+    Example:
+    im = Renderer().draw_problem(p).get_img()
+
+    Use hstack_images() to compose output of individual renderers.
+    """
+
+    def __init__(self):
+        self.points_for_viewport = [Point(0, 0), Point(1, 1)]
+        self.draw_commands = []
+        self.size = 100
+
+    def draw_unit_square(self):
+        self.draw_poly(
+            [Point(0, 0), Point(1, 0), Point(1, 1), Point(0, 1)],
+            color=(60, 60, 60))
+        return self
+
+    def draw_poly(self, poly: List[Point], color=None):
+        """
+        `color` is RGB or RGBA tuple
+        By default random greenish for CCW polys and
+        random reddish for CW polys.
+        """
+        if color is None:
+            area = polygon_area(poly)
+            if area > 0:
+                color = (
+                    random.randrange(100),
+                    random.randrange(150, 256),
+                    random.randrange(100),
+                    100)
+            else:
+                color = (
+                    random.randrange(150, 256),
+                    random.randrange(100),
+                    random.randrange(100),
+                    100)
+
+        for pt in poly:
+            self.update_viewport(pt)
+
+        def draw_command(im):
+            im2 = Image.new('RGBA', (self.size, self.size))
+            draw2 = ImageDraw.Draw(im2)
+            draw2.polygon([self.norm(pt) for pt in poly], fill=color)
+            return Image.alpha_composite(im, im2)
+
+        self.draw_commands.append(draw_command)
+        return self
+
+    def draw_edge(self, pt1: Point, pt2: Point, color=None):
+        if color is None:
+            color = (
+                random.randrange(200, 255),
+                random.randrange(200, 255),
+                random.randrange(200, 255),
+            )
+
+        self.update_viewport(pt1)
+        self.update_viewport(pt2)
+
+        def draw_command(im):
+            draw = ImageDraw.Draw(im)
+            draw.line(
+                [self.norm(pt1), self.norm(pt2)], fill=color)
+            return im
+
+        self.draw_commands.append(draw_command)
+        return self
+
+    def draw_problem(self, p: ioformats.Problem):
+        self.draw_unit_square()
+        for poly in p.silhouette:
+            self.draw_poly(poly)
+        for edge in p.skeleton:
+            self.draw_edge(*edge)
+        return self
+
+    def get_img(self, size=None):
+        if size:
+            self.size = size
+
+        self.min_x = min(p.x for p in self.points_for_viewport)
+        self.max_x = max(p.x for p in self.points_for_viewport)
+        self.min_y = min(p.y for p in self.points_for_viewport)
+        self.max_y = max(p.y for p in self.points_for_viewport)
+
+        # TODO: ensure 1:1 ratio
+        #w = self.max_x - self.min_x
+        #h = self.max_y - self.min_y
+
+        im = Image.new('RGBA', (self.size, self.size))
+        for draw_command in self.draw_commands:
+            im = draw_command(im)
+
+        return im
+
+    def update_viewport(self, pt: Point):
+        self.points_for_viewport.append(pt)
+
+    def norm(self, pt: Point):
+        return (
+            (self.size - 1) * (pt.x - self.min_x) / (self.max_x - self.min_x),
+            self.size - 1 - (self.size - 1) * (pt.y - self.min_y) / (
+                self.max_y - self.min_y),
+        )
 
 
 def render_polys_and_edges(
         polys: List[List[Point]],
         edges: List[Tuple[Point, Point]],
         size=100) -> Image.Image:
-
-    points = sum(polys, [])
-    points.append(Point(0, 0))
-    points.append(Point(1, 1))
-
-    min_x = min(p.x for p in points)
-    max_x = max(p.x for p in points)
-    min_y = min(p.y for p in points)
-    max_y = max(p.y for p in points)
-
-    def norm(x, y):
-        return (
-            (size - 1) * (x - min_x) / (max_x - min_x),
-            size - 1 - (size - 1) * (y - min_y) / (max_y - min_y),
-        )
-
-    im = Image.new('RGBA', (size, size))
-    draw = ImageDraw.Draw(im)
-
-    # Unit square, for scale
-    draw.polygon([
-        norm(0, 0),
-        norm(1, 0),
-        norm(1, 1),
-        norm(0, 1),
-    ], fill=(60, 60, 60))
-
-    for i, poly in enumerate(polys):
-        im2 = Image.new('RGBA', (size, size))
-        draw2 = ImageDraw.Draw(im2)
-
-        area = polygon_area(poly)
-        # CCW is greenish
-        # CW is reddish
-        if area > 0:
-            color = (
-                random.randrange(100),
-                random.randrange(150, 256),
-                random.randrange(100))
-        else:
-            color = (
-                random.randrange(150, 256),
-                random.randrange(100),
-                random.randrange(100))
-
-        draw2.polygon([norm(pt.x, pt.y) for pt in poly], fill=color + (100,))
-
-        im = Image.alpha_composite(im, im2)
-
-    draw = ImageDraw.Draw(im)
-    for pt1, pt2 in edges:
-        draw.line(
-            [norm(pt1.x, pt1.y), norm(pt2.x, pt2.y)], fill=(255, 255, 255))
-
-    return im
+    """This is legacy stuff, just use Renderer directly."""
+    r = Renderer()
+    r.draw_unit_square()
+    for poly in polys:
+        r.draw_poly(poly)
+    for edge in edges:
+        r.draw_edge(*edge)
+    return r.get_img(size)
 
 
 def hstack_images(im1, im2):
@@ -79,7 +143,7 @@ def hstack_images(im1, im2):
 
 
 def main():
-    p = load_problem('problem95')
+    p = ioformats.load_problem('problem95')
     im = render_polys_and_edges(p.silhouette, p.skeleton)
     #im = hstack_images(im, im)
     im.save('hz.png')
--- production/render_tests.py
+++ production/render_tests.py
@@ -1,22 +1,36 @@
 from production import ioformats
-from production import render
+from production.render import Renderer, hstack_images, render_polys_and_edges
 
 
-def test_render_polys_and_edges():
+def test_everything():
     p = ioformats.load_problem('00042')
-    im = render.render_polys_and_edges(p.silhouette, p.skeleton)
-    #im.save('test_render_polys_and_edges.png')
+
+    r = Renderer()
+    r.draw_unit_square()
+    r.draw_poly(p.silhouette[0])
+    im1 = r.get_img()
+
+    im2 = Renderer().draw_problem(p).get_img(size=100)
+
+    im = hstack_images(im1, im2)
+    im.save('test_renderer.png')
+
+
+def test_legacy_render_polys_and_edges():
+    p = ioformats.load_problem('00042')
+    im = render_polys_and_edges(p.silhouette, p.skeleton)
+    im.save('test_render_polys_and_edges.png')
 
 
 def test_hstack_images():
     p = ioformats.load_problem('00042')
-    im1 = render.render_polys_and_edges(p.silhouette, p.skeleton)
+    im1 = render_polys_and_edges(p.silhouette, p.skeleton)
 
     p = ioformats.load_problem('00025')
-    im2 = render.render_polys_and_edges(p.silhouette, p.skeleton)
+    im2 = render_polys_and_edges(p.silhouette, p.skeleton)
 
-    im = render.hstack_images(im1, im2)
-    #im.save('test_hstack_images.png')
+    im = hstack_images(im1, im2)
+    im.save('test_hstack_images.png')
 
 
 if __name__ == '__main__':
