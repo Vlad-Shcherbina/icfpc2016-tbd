2577124122ed32716e8d9ba72a89e40025334002
miker <braneinsane@yandex.com>
2016-08-06 07:20:20 +0300

    random folder fun

---
+++ miker_scratch/random_fold.py
@@ -0,0 +1,32 @@
+from origami_fold import *
+
+import random, types
+
+
+class RandomFolder(types.SimpleNamespace):
+	
+	spread = 10 # int
+	count = 5
+	
+	def random_int(self):
+		d = self.spread
+		return int(random.random() * 2 * d - d)
+	
+	def random_point(self):
+		r = self.random_int
+		t = (r(), r())
+		return make_point(t)
+	
+	def random_line(self):
+		r = self.random_point
+		t = (r(), r())
+		return Edge(*t)
+		
+	def fold(self):
+		polys = [unitsq]
+		for i in range(self.count):
+			e = None
+			while not e or e.is_zero:
+				e = self.random_line()
+			polys = fold(polys, e)
+		return polys
