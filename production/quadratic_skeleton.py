0e1913d6c91cbb64c575cb97e6203e1fe32992fd
peluche <me@peluche.im>
2016-08-07 01:39:40 +0200

    Submit quad solution with 3/4 rotation

--- production/quadratic_skeleton.py
+++ production/quadratic_skeleton.py
@@ -1,7 +1,7 @@
 from production import render
 from production.cg import Point
 from production import ioformats
-from production.origami_fold import fold, write_fold, unitsq, Edge, polygon_points
+from production.origami_fold import fold, write_fold, unitsq_f, Edge, polygon_points
 from fractions import Fraction
 import random
 import itertools
@@ -23,20 +23,28 @@ def fold_at_45(polys, eps=Fraction(-1, 2)):
     return fold(polys, Edge(Point(botr.x - 1, botr.y + eps),
                             Point(botr.x, botr.y + 1 + eps)))
 
+def fold_at_3_4(polys, eps=Fraction(-1, 2)):
+    points = get_points(polys[-1])
+    botr = min(points, key=lambda p: (p.y, -p.x))
+    return fold(polys, Edge(Point(botr.x - 1, botr.y + eps),
+                            Point(botr.x + 3, botr.y + 1 + eps + 4)))
+
 def fold_at_45_2(polys):
-    eps = Fraction(1, 50)
+    # eps = Fraction(1, 50) # tweek me for 3/4 rot
+    eps = Fraction(1, 5)
     return fold(polys, Edge(Point(Fraction(1), Fraction(1, 2) + eps),
                             Point(Fraction(3, 2), Fraction(0) + eps)))
 
 def quadratic_skeleton(n):
     flap_size = Fraction(1, n + 4)
     delta_fold_width = flap_size / (2 * n ** 2)
-    polys = [unitsq]
+    polys = [unitsq_f()]
     print(polys[-1])
     for i in range(n):
         e = make_flap(polys[-1], i, flap_size, delta_fold_width)
         polys = fold(polys, e)
-    polys = fold_at_45(polys)
+    # polys = fold_at_45(polys)
+    polys = fold_at_3_4(polys)
     polys = fold_at_45_2(polys)
     return polys
 
@@ -44,8 +52,8 @@ if __name__ == '__main__':
     from production.render import render_polys_and_edges
 
     sizes = []
-    for i in range(1, 15):
-    # for i in [11]:
+    # for i in range(1, 15):
+    for i in [10]:
         polys = quadratic_skeleton(n=i)
         print('*' * 50)
         res = write_fold(polys)
