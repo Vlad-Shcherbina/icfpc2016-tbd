aa8a844464098cda0b3fcb1dc6a2dab96b3a062d
Jonn Mostovoy <amarr.industrial@gmail.com>
2016-08-07 21:53:20 +0300

    Start solving 2766 - 3766 to use up API tries for these 7 minutes

--- production/approximate_solver_convex_hull.py
+++ production/approximate_solver_convex_hull.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
     # 4461
     # problems = [989, 1456, 2606, 3560, 3852, 3854, 3929, 4008, 4010, 4229, 4236, 4239, 4861, 5195, 5199, 5293, 5311, 5724, 5726, 5907, 5933, 5949]
     #problems = [5195, 5199, 5293, 5311, 5724, 5726, 5907, 5949]
-    problems = range(2666, 2666+100)
+    problems = range(2766, 3766)
 
     #parser = argparse.ArgumentParser(description='Compute approximate solution')
     #parser.add_argument(dest='prob_id')
