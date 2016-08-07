96447445187e06addeff7d285628e9f11d98c4ff
Jonn Mostovoy <amarr.industrial@gmail.com>
2016-08-07 20:34:16 +0300

    Implement find_original_solution[_file] functions.

--- production/dedup.py
+++ production/dedup.py
@@ -1,5 +1,44 @@
+import os
+import json
+from pprint import pprint
+
+def to_five_digits(x):
+    n = len(str(x))
+    z = '0' * (5 - n)
+    return z + str(x)
+
+def five_digits_to_solution_file(five_digits):
+    path = os.path.join(__file__, '..', '..', 'solutions', 'solved_' + five_digits + '.txt')
+    return os.path.normpath(path)
+
+def find_original_solution_file(problem_id):
+    maybe_orig = find_original_solution(problem_id)
+    if maybe_orig == None:
+        return maybe_orig
+    else:
+        return five_digits_to_solution_file(to_five_digits(maybe_orig))
+
+def find_original_solution(problem_id):
+    pid  = to_five_digits(problem_id)
+    path = os.path.join(__file__, '..', '..', 'problems', '__problem_hashes')
+    path = os.path.normpath(path)
+    mapping = {}
+    with open(path) as f:
+        mapping = json.load(f)
+    candidates = []
+    for k, v in mapping.items():
+        if pid in v:
+            candidates = v
+            break
+    for candidate in candidates:
+        if os.path.exists(five_digits_to_solution_file(candidate)):
+            return int(candidate)
+
 def main():
-	return
+    pprint(find_original_solution(63))
+    pprint(find_original_solution_file(63))
+    pprint(find_original_solution(666))
+    pprint(find_original_solution_file(666))
 
 if __name__ == "__main__":
     main()
