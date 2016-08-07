5f4f4d95cf77363467343c4cf0e25c3919f12f64
Jonn Mostovoy <amarr.industrial@gmail.com>
2016-08-07 18:49:58 +0300

    [Automated]: add maps and rehash

--- problems/scrape_maps.sh
+++ problems/scrape_maps.sh
@@ -1,7 +1,13 @@
+set -x
+
 git pull origin master -f
 python3 scrape_maps.py
 (for x in 0*txt; do sha224sum $x; done) > _problem_hashes
 python3 jsonify_problem_hashes.py > _problem_hashes
+cd ../manpages_scratch/
+python3 produce_stats_of_our_problems.py
+python3 _produce_stats_of_all_problems.py
+cd ../problems/
 git add .
 git commit -am '[Automated]: add maps and rehash'
 git push origin master
