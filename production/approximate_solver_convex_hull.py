4f8011dd53e19b189970e169a672f9120153acf3
Jonn Mostovoy <amarr.industrial@gmail.com>
2016-08-08 02:06:13 +0300

    [Automated]: add maps and rehash

--- production/approximate_solver_convex_hull.py
+++ production/approximate_solver_convex_hull.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
     # 4461
     # problems = [989, 1456, 2606, 3560, 3852, 3854, 3929, 4008, 4010, 4229, 4236, 4239, 4861, 5195, 5199, 5293, 5311, 5724, 5726, 5907, 5933, 5949]
     #problems = [5195, 5199, 5293, 5311, 5724, 5726, 5907, 5949]
-    problems = uns.x()[-900:]
+    problems = uns.x()[49:-900:2]
 
 
     #parser = argparse.ArgumentParser(description='Compute approximate solution')
