d16e0f79e46951ca759cf04442667c1e84dc1119
peluche <me@peluche.im>
2016-08-07 15:48:08 +0200

    Submitted problems until the end of contest

--- production/quadratic_skeleton.py
+++ production/quadratic_skeleton.py
@@ -9,13 +9,6 @@ import itertools
 def get_points(poly):
     return list(itertools.chain.from_iterable(poly.edges))
 
-# def make_flap(poly, i, flap_size, delta_fold_width):
-#     points = get_points(poly)
-#     botl = min(points, key=lambda p: (p.y, p.x))
-#     delta = 0 if i else flap_size
-#     delta += delta_fold_width * i
-#     return Edge(Point(botl.x + delta + flap_size, botl.y),
-#                 Point(botl.x + delta, botl.y + 1))
 def make_flap(poly, i, flap_size, delta_fold_width):
     points = get_points(poly)
     botl = min(points, key=lambda p: (p.y, p.x))
@@ -24,7 +17,6 @@ def make_flap(poly, i, flap_size, delta_fold_width):
     return Edge(Point(botl.x + delta + flap_size, botl.y),
                 Point(botl.x + delta, botl.y + 1))
 
-# def fold_at_45(polys, eps=Fraction(-1, 2)):
 def fold_at_45(polys, eps=Fraction(-1, 2)):
     points = get_points(polys[-1])
     botr = min(points, key=lambda p: (p.y, -p.x))
@@ -45,18 +37,11 @@ def fold_at_4_3(polys, eps=Fraction(-1, 2)):
                             Point(botr.x + 4, botr.y + 1 + eps + 3)))
 
 def fold_180(polys, flap_size, eps=Fraction(-1, 2)):
-    # points = get_points(polys[-1])
-    # botr = min(points, key=lambda p: (p.y, -p.x))
-    # return fold(polys, Edge(Point(botr.x - 1, botr.y + eps),
-    #                         Point(botr.x + 4, botr.y + 1 + eps + 3)))
-    return fold(polys, Edge(Point(1 - Fraction(flap_size), Fraction(0)),
-                            Point(1 - Fraction(flap_size), Fraction(1))))
-
-# def fold_at_3_4_2(polys):
-#     # eps = Fraction(1, 50) # tweek me for 3/4 rot
-#     eps = Fraction(1, 6) 
-#     return fold(polys, Edge(Point(Fraction(1), Fraction(1, 2) + eps),
-#                             Point(Fraction(3, 2) + 3, Fraction(0) + eps + 4)))
+    return fold(polys, Edge(Point(1 - Fraction(flap_size), Fraction(1)),
+                            Point(1 - Fraction(flap_size), Fraction(0))))
+def fold_180_2(polys, flap_size, eps=Fraction(-1, 95)):
+    return fold(polys, Edge(Point(1 - 3 * Fraction(flap_size) + eps, Fraction(0)),
+                            Point(1 - 3 * Fraction(flap_size) + eps, Fraction(1))))
 
 
 def fold_at_45_2(polys):
@@ -82,7 +67,7 @@ def quadratic_skeleton(n):
     # polys = fold_at_3_4(polys)
     polys = fold_at_4_3(polys)
     polys = fold_180(polys, flap_size)
-    # polys = fold_at_3_4_2(polys)
+    polys = fold_180_2(polys, flap_size)
     # polys = fold_at_45_2(polys)
     return polys
 
@@ -91,7 +76,8 @@ if __name__ == '__main__':
 
     sizes = []
     # for i in range(1, 15):
-    for i in [10]:
+    # for i in [10]:
+    for i in [8]:
         polys = quadratic_skeleton(n=i)
         print('*' * 50)
         res = write_fold(polys)
