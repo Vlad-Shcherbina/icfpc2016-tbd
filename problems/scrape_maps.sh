a03767cc723a3f640137c4c3504a8df3f810180f
Jonn Mostovoy <amarr.industrial@gmail.com>
2016-08-07 18:51:18 +0300

    [Automated]: add maps and rehash

--- problems/scrape_maps.sh
+++ problems/scrape_maps.sh
@@ -5,8 +5,8 @@ python3 scrape_maps.py
 (for x in 0*txt; do sha224sum $x; done) > _problem_hashes
 python3 jsonify_problem_hashes.py > _problem_hashes
 cd ../manpages_scratch/
-python3 produce_stats_of_our_problems.py
-python3 _produce_stats_of_all_problems.py
+python3 produce_stats_of_our_problems.py  > our_problems.txt
+python3 _produce_stats_of_all_problems.py > all_problems.txt
 cd ../problems/
 git add .
 git commit -am '[Automated]: add maps and rehash'
