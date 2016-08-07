5c9c5fabf1f0c575e5a18b95fa12d663fc7e7329
Jonn Mostovoy <amarr.industrial@gmail.com>
2016-08-07 19:26:35 +0300

    Hashified

--- problems/hashify_problems.py
+++ problems/hashify_problems.py
@@ -1,1 +1,12 @@
-print('.')
+import json
+from pprint import pprint
+
+d = None
+
+with open('__problem_hashes') as f:
+    d = json.load(f)
+
+for k, v in d.items():
+    with open( v[0] + '.txt' ) as f:
+        with open( 'hashed/' + k + '.txt', 'w+' ) as g:
+            g.write(f.read())
